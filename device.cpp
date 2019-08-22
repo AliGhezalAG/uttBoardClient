0c994e8b4b636a22672d2dad81e8d2aec969fb53
Ali Ghezal <ali.ghezal.ag@gmail.com>
2019-08-22 14:58:41 +0200

    acquisitiopn balance OK

--- device.cpp
+++ device.cpp
@@ -9,7 +9,7 @@ Device::Device()
 
     connect(deviceDiscoveryAgent, SIGNAL(deviceDiscovered(QBluetoothDeviceInfo)),
             this, SLOT(addDevice(QBluetoothDeviceInfo)));
-    connect(deviceDiscoveryAgent, SIGNAL(finished()), this, SLOT(scanFinished()));
+    connect(deviceDiscoveryAgent, SIGNAL(finished()), this, SLOT(deviceScanFinished()));
 }
 
 Device::~Device()
@@ -30,7 +30,7 @@ void Device::startScan()
     deviceDiscoveryAgent->start();
 }
 
-void Device::scanFinished()
+void Device::deviceScanFinished()
 {
     qInfo() << "scan finished!";
 
@@ -72,20 +72,94 @@ void Device::startClient()
     socket->connectToService(uttBoardAddress, 1);
     qDebug() << "ConnectToService done";
 
-
-    connect(socket, SIGNAL(readyRead()), this, SLOT(readSocket()));
-    connect(socket, SIGNAL(connected()), this, SLOT(requestData()));
-    connect(socket, SIGNAL(disconnected()), this, SIGNAL(processReceivedData()));
+    connect(socket, &QBluetoothSocket::connected, this, &Device::requestData);
+    connect(socket, &QBluetoothSocket::readyRead, this, &Device::readSocket);
+    connect(this, &Device::dataReceived, this, &Device::processReceivedData);
+    connect(socket, &QBluetoothSocket::disconnected, this, &Device::deviceDisconnected);
 }
 
 void Device::processReceivedData()
 {
     qDebug() << "Processing received data...";
+    extractPackets();
+    acquisition = new Acquisition();
+    acquisition->serialNumber = byteArrayToInt(openingPacket.mid(0, 4));
+    acquisition->samplingFrequency = byteArrayToInt(openingPacket.mid(4, 1));
+    acquisition->scaleMode = "Normal mode";
+    acquisition->versionNumber = byteArrayToInt(openingPacket.mid(6, 1));
+    acquisition->batteryLevel = byteArrayToInt(openingPacket.mid(7, 1));
+    acquisition->distanceBetweenX = byteArrayToInt(openingPacket.mid(8, 2));
+    acquisition->distanceBetweenY = byteArrayToInt(openingPacket.mid(10, 2));
+
+    acquisition->recordNumber = byteArrayToInt(closingPacket.mid(0, 4));
+    acquisition->errorCode = byteArrayToInt(closingPacket.mid(4, 2));
+    acquisition->weightValue = byteArrayToInt(closingPacket.mid(6, 2));
+    acquisition->impedance = byteArrayToInt(closingPacket.mid(8, 2));
+
+    QListIterator<QByteArray> i(recordPacketList);
+    QByteArray currentPacket;
+
+    while (i.hasNext()){
+        currentPacket = i.next();
+        Packet *packet = new Packet();
+        packet->recordNumber = byteArrayToInt(currentPacket.mid(0, 4));
+        packet->timeStamp = packet->recordNumber / acquisition->samplingFrequency;
+        QList<double> records = {};
+        packet->pressureMeasures.append(QJsonValue(byteArrayToDouble(currentPacket.mid(4, 2))));
+        packet->pressureMeasures.append(QJsonValue(byteArrayToDouble(currentPacket.mid(6, 2))));
+        packet->pressureMeasures.append(QJsonValue(byteArrayToDouble(currentPacket.mid(8, 2))));
+        packet->pressureMeasures.append(QJsonValue(byteArrayToDouble(currentPacket.mid(10, 2))));
+        acquisition->measurePackets.append(packet->toJson());
+    }
+
+    auto doc = QJsonDocument(acquisition->toJson());
+    logFile.open ("results.json", ios::out | ios::app);
+    logFile << doc.toJson().constData();
+    logFile.close();
+
+    qInfo() << acquisition->toJson();
 }
+
+int Device::byteArrayToInt(const QByteArray &bytes){
+    QByteArray bytesToHex = bytes.toHex(0);
+    bool ok;
+    int hex = bytesToHex.toInt(&ok, 16);
+    return hex;
+}
+
+double Device::byteArrayToDouble(const QByteArray &bytes){
+    QByteArray bytesToHex = bytes.toHex(0);
+    double hex = bytesToHex.toDouble();
+    return hex;
+}
+
+void Device::extractPackets()
+{
+    qInfo() << "Extracting packets...";
+
+    QByteArray openingPacketStartPrefix = QByteArray::fromHex("5049");
+    QByteArray openingPacketEndPrefix = QByteArray::fromHex("4950");
+    int openingPacketStartIndex = receivedData.indexOf(openingPacketStartPrefix)+2;
+    int openingPacketEndIndex = receivedData.indexOf(openingPacketEndPrefix);
+    openingPacket = receivedData.mid(openingPacketStartIndex, PACKET_SIZE);
+
+    QByteArray closingPacketStartPrefix = QByteArray::fromHex("5052");
+    int closingPacketStartIndex = receivedData.indexOf(closingPacketStartPrefix)+2;
+    closingPacket = receivedData.mid(closingPacketStartIndex, PACKET_SIZE);
+
+    QByteArray recordPacket = receivedData.mid(openingPacketEndIndex+2, closingPacketStartIndex-openingPacketEndIndex-4);
+    recordPacketList = {};
+    int recordPacketStartIndex = 2;
+
+    while(recordPacketStartIndex < recordPacket.size()){
+        recordPacketList.append(recordPacket.mid(recordPacketStartIndex, PACKET_SIZE));
+        recordPacketStartIndex += 16;
+    }
+}
+
 void Device::requestData()
 {
     qDebug() << "Requesting data...";
-
     QByteArray startPrefix = QByteArray::fromHex("5253");
     socket->write(startPrefix, 1024);
 }
@@ -94,7 +168,13 @@ void Device::readSocket()
 {
     qDebug() << "Data received...";
     QByteArray data = socket->readAll();
+    receivedData.append(data);
     qDebug() << data;
+    if (data.endsWith(QByteArray::fromHex("5250"))){
+        qInfo() << "this is the end!!";
+        emit dataReceived();
+    }
+
     QByteArray endPrefix = QByteArray::fromHex("5350");
     socket->write(endPrefix, 1024);
 }
@@ -111,4 +191,8 @@ void Device::addService(const QBluetoothServiceInfo &info)
         line.append("\n\t" + info.serviceProvider());
     discoveredServicesList.append(info);
     qInfo() << line;
+}
+
+void Device::deviceDisconnected(){
+    qInfo() << "Disconnected!";
 }
--- device.h
+++ device.h
@@ -10,13 +10,74 @@
 #include <QBluetoothSocket>
 #include <QBluetoothServiceDiscoveryAgent>
 #include <QDebug>
+#include <QtCore>
+
+#include <iostream>
+#include <fstream>
 
 QT_FORWARD_DECLARE_CLASS(QBluetoothDeviceDiscoveryAgent)
 QT_FORWARD_DECLARE_CLASS(QBluetoothDeviceInfo)
 
 QT_USE_NAMESPACE
 
-#define UTT_BOARD_ADDRESS              "94:21:97:60:14:D6"
+using namespace std;
+
+#define UTT_BOARD_ADDRESS               "94:21:97:60:14:D6"
+#define PACKET_SIZE                     12
+
+struct Packet {
+    int recordNumber;
+    double timeStamp;
+    QJsonArray pressureMeasures;
+
+    QJsonObject toJson() const {
+        return {
+            {"recordNumber", recordNumber},
+            {"timeStamp", timeStamp},
+            {"pressureMeasures", pressureMeasures}
+        };
+    }
+};
+
+struct Acquisition {
+    int          serialNumber;
+    int          samplingFrequency;
+    QString      frequencyUnit;
+    QString      scaleMode;
+    double        versionNumber;
+    int          batteryLevel;
+    int          distanceBetweenX;
+    int          distanceBetweenY;
+    QString      distanceUnit;
+    QString      pressureOrder;
+    QString      pressureUnit;
+    int          recordNumber;
+    int          errorCode;
+    int          weightValue;
+    int          impedance;
+    QJsonArray   measurePackets;
+
+    QJsonObject toJson() const {
+        return {
+            {"serialNumber", serialNumber},
+            {"samplingFrequency", samplingFrequency},
+            {"frequencyUnit", frequencyUnit} ,
+            {"scaleMode", scaleMode},
+            {"versionNumber", versionNumber},
+            {"batteryLevel", batteryLevel},
+            {"distanceBetweenX", distanceBetweenX},
+            {"distanceBetweenY", distanceBetweenY} ,
+            {"distanceUnit", distanceUnit},
+            {"pressureOrder", pressureOrder},
+            {"pressureUnit", pressureUnit},
+            {"recordNumber", recordNumber},
+            {"errorCode", errorCode} ,
+            {"weightValue", weightValue},
+            {"impedance", impedance},
+            {"measurePackets", measurePackets}
+        };
+    }
+};
 
 class Device : public QObject
 {
@@ -28,21 +89,34 @@ public:
     void startScan();
 
 private slots:
-    void addDevice(const QBluetoothDeviceInfo&);
     void startClient();
-    void scanFinished();
+    void deviceScanFinished();
+    void addDevice(const QBluetoothDeviceInfo&);
     void addService(const QBluetoothServiceInfo &info);
     void readSocket();
     void requestData();
     void processReceivedData();
+    void deviceDisconnected();
 
 private:
-    QList<QString> discoveredDevicesList;
-    QList<QBluetoothServiceInfo> discoveredServicesList;
-    QBluetoothSocket *socket;
+    QList<QString>                  discoveredDevicesList;
+    QList<QBluetoothServiceInfo>    discoveredServicesList;
+    QBluetoothSocket                *socket;
     QBluetoothServiceDiscoveryAgent *serviceDiscoveryAgent;
-    QBluetoothDeviceDiscoveryAgent *deviceDiscoveryAgent;
-    QBluetoothLocalDevice *localDevice;
+    QBluetoothDeviceDiscoveryAgent  *deviceDiscoveryAgent;
+    QBluetoothLocalDevice           *localDevice;
+    QByteArray                      receivedData;
+    QByteArray                      openingPacket;
+    QByteArray                      closingPacket;
+    QList<QByteArray>               recordPacketList;
+    Acquisition                     *acquisition;
+    ofstream                        logFile;
+    void extractPackets();
+    int byteArrayToInt(const QByteArray &bytes);
+    double byteArrayToDouble(const QByteArray &bytes);
+
+signals:
+    void dataReceived();
 
 };
 
